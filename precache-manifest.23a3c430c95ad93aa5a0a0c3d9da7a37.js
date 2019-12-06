cdf67204d2a8cd482ede29132605b46a1396e453
awamas <tawatetb04@gmail.com>
2019-12-06 15:15:31 +0100

    Updates

---
+++ precache-manifest.23a3c430c95ad93aa5a0a0c3d9da7a37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502bfc9280cbe81fb1b53fa926b05d82",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5f17b2e9f478caaf502b",
+    "url": "/monsters-rolodex/static/css/main.12813e31.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "5f17b2e9f478caaf502b",
+    "url": "/monsters-rolodex/static/js/main.3e9ccad5.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
